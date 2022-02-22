620f961b55dfe6c19243a9161b33389a9e76fa51
deuro-ki <hero14yki1986@gmail.com>
2022-02-23 04:20:52 +0900

    Finish practice form-field

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def top
   end
+
+  def form_field
+    #@pages = Page.all
+  end
+
+  def link_to
+    @title = "本日"
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#top'
+  get 'pages/form_field'
+  get 'pages/link_to'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
